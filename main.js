fee9757f88a24b4e5c8cb70c66e24a2b49af7bba
Louay-Trabelsi <louayt250@gmail.com>
2025-01-08 11:49:14 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+var mangaList = [
+    {
+      name: "One Piece",
+      chapter: 1095,
+      src: "https://upload.wikimedia.org/wikipedia/en/3/3c/One_Piece_Logo.png"
+    },
+    {
+      name: "Naruto",
+      chapter: 700,
+      src: "https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg"
+    },
+    {
+      name: "Attack on Titan",
+      chapter: 139,
+      src: "https://upload.wikimedia.org/wikipedia/en/4/4a/Attack_on_Titan_vol1_cover.jpg"
+    },
+    {
+      name: "My Hero Academia",
+      chapter: 401,
+      src: "https://upload.wikimedia.org/wikipedia/en/0/03/My_Hero_Academia_Volume_1_cover.jpg"
+    },
+    {
+      name: "Demon Slayer",
+      chapter: 205,
+      src: "https://upload.wikimedia.org/wikipedia/en/1/1b/Demon_Slayer_-_Kimetsu_no_Yaiba%2C_volume_1.jpg"
+    },
+    {
+      name: "Jujutsu Kaisen",
+      chapter: 240,
+      src: "https://upload.wikimedia.org/wikipedia/en/2/2d/Jujutsu_Kaisen_vol_1.png"
+    },
+    {
+      name: "Chainsaw Man",
+      chapter: 143,
+      src: "https://upload.wikimedia.org/wikipedia/en/a/a8/Chainsaw_Man_vol_1.png"
+    },
+    {
+      name: "Bleach",
+      chapter: 686,
+      src: "https://upload.wikimedia.org/wikipedia/en/7/72/Bleach_cover_01.jpg"
+    },
+    {
+      name: "Tokyo Revengers",
+      chapter: 278,
+      src: "https://upload.wikimedia.org/wikipedia/en/e/ed/Tokyo_Revengers_vol_1.png"
+    },
+    {
+      name: "Black Clover",
+      chapter: 372,
+      src: "https://upload.wikimedia.org/wikipedia/en/2/21/Black_Clover_volume_1_cover.jpg"
+    }
+  ];
+  mangaList.map(function(){
+    $('mainpage').append(`
+        `)
+  })
+  
